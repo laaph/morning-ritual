27d12730cdfc3f6f325d288391a7d531200c0f81
Dietrich Epp <depp@zdome.net>
2016-01-30 14:57:31 -0800

    track game score and display on screen

--- MorningRitual/Assets/Scripts/GameManager.cs
+++ MorningRitual/Assets/Scripts/GameManager.cs
@@ -2,14 +2,47 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	private static GameManager instanceValue;
+
+	/// <summary>
+	/// Get the global GameManager instance.
+	/// </summary>
+	public static GameManager Instance {
+		get {
+			if (!instanceValue) {
+				var objs = FindObjectsOfType<GameManager>();
+				Debug.Assert(objs.Length == 1);
+				instanceValue = objs[0];
+			}
+			return instanceValue;
+		}
+	}
+
+	public delegate void Action();
+	public event Action ScoreDidChange;
+	private int scoreValue;
+	public int Score {
+		get { return this.scoreValue; }
+		set {
+			if (value != this.scoreValue) {
+				this.scoreValue = value;
+				this.ScoreDidChange.Invoke();
+			}
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		this.Score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void OnDestroy() {
+		if (this == instanceValue) {
+			instanceValue = null;
+		}
 	}
 }
---
+++ MorningRitual/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		this.text = GetComponent<Text>();
+		GameManager.Instance.ScoreDidChange += this.OnScoreChanged;
+		this.OnScoreChanged();
+	}
+	
+	void OnScoreChanged() {
+		this.text.text = string.Format("Score: {0}", GameManager.Instance.Score);
+	}
+}
