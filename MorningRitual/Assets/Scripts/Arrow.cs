c8aa9e70215ef2a0139b3c43863203cd59618551
Jethro <jethro@laaph.com>
2016-01-30 14:42:13 -0800

    Initial moving, only set up between bedroom and bathroom

---
+++ MorningRitual/Assets/Scripts/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	
+
+public class Arrow : MonoBehaviour {
+
+	public GameObject nextRoom;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		gameObject.SetActive(false);
+		nextRoom.SetActive(true);
+		Camera.main.transform.position = nextRoom.GetComponentInChildren<RoomObjectManager>().cameraPosition;
+	}
+
+}
