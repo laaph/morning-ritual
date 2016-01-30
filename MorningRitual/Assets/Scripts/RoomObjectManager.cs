c32df08cda37f998b68ec20c263921eeb6a7ddbe
Jethro <jethro@laaph.com>
2016-01-30 15:26:40 -0800

    Cleaned up movement, still undone with only top house traversable

--- MorningRitual/Assets/Scripts/RoomObjectManager.cs
+++ MorningRitual/Assets/Scripts/RoomObjectManager.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class RoomObjectManager : MonoBehaviour {
 
 	public Vector3 cameraPosition;
+	public bool isInitialRoom = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		if(isInitialRoom) {
+			Camera.main.transform.position = cameraPosition;
+			gameObject.SetActive(true);
+		} else {
+			gameObject.SetActive(false);
+		}
 	}
 	
 	// Update is called once per frame
