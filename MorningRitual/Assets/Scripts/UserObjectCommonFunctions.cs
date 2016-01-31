7363c756f9be1505e86cb2b4b98828aa551c0fc6
Jethro <jethro@laaph.com>
2016-01-30 16:31:52 -0800

    Added mouseover effects and a few objects to test upon

---
+++ MorningRitual/Assets/Scripts/UserObjectCommonFunctions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserObjectCommonFunctions : MonoBehaviour {
+	Renderer rend;
+	public Color glowColor = Color.red;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+        rend.material.color = glowColor;
+    }
+    void OnMouseOver() {
+       // rend.material.color -= new Color(0.1F, 0, 0);
+    }
+    void OnMouseExit() {
+        rend.material.color = Color.white;
+    }
+}
