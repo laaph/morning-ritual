1811dd4c96b3036c4502ccb38a3204410dd14dab
Dietrich Epp <depp@zdome.net>
2016-01-30 20:41:23 -0800

    script coffee interactions

---
+++ MorningRitual/Assets/Scripts/CoffeePot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoffeePot : MonoBehaviour {
+	public Mug mug;
+
+	void Start() {
+		if (!mug) {
+			Debug.LogError("Coffee pot is not connected to mug");
+		}
+	}
+
+	void OnMouseDown() {
+		if (!mug) {
+			return;
+		}
+		mug.AddCoffee();
+	}
+}
