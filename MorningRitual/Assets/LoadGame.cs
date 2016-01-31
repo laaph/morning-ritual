0eb8b6f53295d6cd0a9400b67c04aa2b55aef7bb
Chris M <chris@techfocus.net>
2016-01-31 15:12:15 -0800

    added beginning and end scenes

---
+++ MorningRitual/Assets/LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class StartGame : MonoBehaviour {
+	public string Scene;
+	// Use this for initialization
+	public void  LevelStart(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
+
+	}
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ MorningRitual/Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ReturnToTitle : MonoBehaviour {
+	public string Scene = "Opening";
+	// Use this for initialization
+	public void  GoToTitle(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
+	}
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
