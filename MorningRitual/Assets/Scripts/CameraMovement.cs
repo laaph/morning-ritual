1493d3748e52ff3fd4c2a07167c302b96ad52661
Dietrich Epp <depp@zdome.net>
2016-01-30 23:09:14 -0800

    smoother camera movement

--- MorningRitual/Assets/Scripts/CameraMovement.cs
+++ MorningRitual/Assets/Scripts/CameraMovement.cs
@@ -9,20 +9,22 @@ public class CameraMovement : MonoBehaviour {
 	bool ismoving = false;
 	private Vector3 nextCameraPosition;
 	private Vector3 cameraStart;
-	//	public ref CamVelocity = 10.0;
-	public float smoothTime = 0.3F;
+	public float speed = 30.0f;
+	public float smoothing = 1.0f;
 	private float startTime;
+	private float deltaTime;
 	private Action callback;
 
 	// Update is called once per frame
 	void Update () {
 		if (ismoving) {
-			float t = (Time.time - startTime) / smoothTime;
+			float t = (Time.time - startTime) / deltaTime;
 			if (t >= 1) {
 				ismoving = false;
 				Camera.main.transform.position = nextCameraPosition;
 				this.callback ();
 			} else {
+				t = t * (1 - smoothing) + (0.5f - 0.5f * Mathf.Cos(t * Mathf.PI)) * smoothing;
 				Camera.main.transform.position = Vector3.Lerp (cameraStart, nextCameraPosition, t);
 			}
 		}
@@ -32,7 +34,9 @@ public class CameraMovement : MonoBehaviour {
 		ismoving = true;
 		nextCameraPosition = target;
 		cameraStart = Camera.main.transform.position;
+		float dist = Vector3.Distance(cameraStart, nextCameraPosition);
 		startTime = Time.time;
+		deltaTime = dist / speed;
 		this.callback = callback;
 	}
 }
