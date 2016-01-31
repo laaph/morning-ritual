491f8ae6c094fe7ea2ee26d60f5e962f7133ba3d
Jethro <jethro@laaph.com>
2016-01-30 19:04:24 -0800

    initial work on gale getting out of bed

---
+++ MorningRitual/Assets/Scripts/BedroomArrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BedroomArrow : MonoBehaviour {
+	public GameObject galeSleeping;
+	public GameObject galeStanding;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		galeSleeping.SetActive(false);
+		galeStanding.SetActive(true);
+	}
+}
