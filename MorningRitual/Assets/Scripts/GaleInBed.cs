5688772d755be109e3a9270703ccd103d8405ce4
Chris M <chris@techfocus.net>
2016-01-31 17:14:23 -0800

    added waking message

--- MorningRitual/Assets/Scripts/GaleInBed.cs
+++ MorningRitual/Assets/Scripts/GaleInBed.cs
@@ -8,12 +8,15 @@ public class GaleInBed : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		timeStart = Time.time;
+		GameManager.Instance.ShowMessage("mumble mumble...\nI need to get out by 7:40");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Time.time > timeStart + frequency) {
 			Bubble.DisplayMessage("zzzz", transform.position + new Vector3(3f, 1f, 0f));
+
+
 			timeStart = Time.time;
 		}
 	}
