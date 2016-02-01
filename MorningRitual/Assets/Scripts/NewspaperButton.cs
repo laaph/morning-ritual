b345b0949d07bff8091f58d6a69ab00af04dcb0c
Jethro <jethro@laaph.com>
2016-01-31 17:59:14 -0800

    Forgot to add newspaper button scripts

---
+++ MorningRitual/Assets/Scripts/NewspaperButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewspaperButton : MonoBehaviour {
+	public GameObject newspaperLate;
+	public GameObject newspaperCoffee;
+
+	// Use this for initialization
+	void Start () {
+		if (!this.newspaperLate) {
+			Debug.LogError("NewspaperButton missing late article");
+		}
+		if (!this.newspaperCoffee) {
+			Debug.LogError("NewspaperButton missing coffee article");
+		}		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		if(GameManager.Instance.hadCoffee) {
+			newspaperLate.SetActive(true);
+		} else {
+			newspaperCoffee.SetActive(true);
+		}
+		gameObject.SetActive(false);
+	}
+
+}
